05f5d2d871cf17e8fde5d24033a5cff1f94fadbc
ejaanck <149357949+ejaanck@users.noreply.github.com>
2024-02-21 13:09:02 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID", "25639252"))
 API_HASH = getenv("API_HASH", "42db0fd56c51ff2b94cf064838eba7c1")
 
-BOT_TOKEN = getenv("BOT_TOKEN", ")
+BOT_TOKEN = getenv("BOT_TOKEN", "7121201474:AAGMnBXMtl_quq7h_Dm9wbqMJjtT4Eny1YE")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://jembutan1:1234@cluster0.w186hoa.mongodb.net/?retryWrites=true&w=majority")
 
 OWNER_ID = int(getenv("OWNER_ID", 5061180769))
